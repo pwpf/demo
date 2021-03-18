e521fa613e78709281eb781081e91135df287c8a
Sławomir Kaleta <dusta@users.noreply.github.com>
2021-03-18 14:33:51 +0100

    Resolved #10, Wrong Vendor path

--- uninstall.php
+++ uninstall.php
@@ -33,7 +33,7 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 /**
  * Load PSR-4
  */
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor_prefixed/autoload.php';
 
 // Bootstrap Plugin without router.
 new Plugin_Name();
